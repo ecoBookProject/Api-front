e4bbe7cec5c2fa662d8f9ca92ddbdb80c240b260
unknown <caioviniciusg.1000@gmail.com>
2021-10-04 11:52:17 -0300

    [feature: Caio - Task: update project]

---
+++ src/app/models/BookModel.ts
@@ -0,0 +1,21 @@
+import { CategoryModel } from "./CategoryModel";
+import { UserModel } from "./UserModel";
+
+export class BookModel{
+    idProduct: number;
+    title: string;
+    description: string;
+    price: number;
+    author: string;
+    year: number;
+    inventory: number;
+    language: string;
+    isbn: number;
+    ean: number;
+    country: string;
+    publisher: string;
+    format: string;
+    pages: number;
+    category: CategoryModel;
+    users: UserModel;
+}
