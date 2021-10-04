a5598e88e8ed188204e8be9259db2454687264a6
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-10-04 14:57:09 -0300

    [feature Gabriela - Task: login authentication]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'cadastro', component: CadastrarComponent },
   { path: 'login', component: LoginComponent }
