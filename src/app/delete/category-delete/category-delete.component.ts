bd5651f98ba31826e99c38cd613eac9d8d78dd85
NicXavier <notedenic@gmail.com>
2021-10-12 14:12:22 -0300

    category method delete created

--- src/app/delete/category-delete/category-delete.component.ts
+++ src/app/delete/category-delete/category-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoryModel } from 'src/app/models/CategoryModel';
+import { CategoryService } from 'src/app/services/category.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-category-delete',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryDeleteComponent implements OnInit {
 
-  constructor() { }
+  category: CategoryModel = new CategoryModel()
+  idCategory: number
 
-  ngOnInit(): void {
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == '') {
+      this.router.navigate(['/login'])
+    }
+    let idCategory = this.route.snapshot.params['idCategory']
+    this.findByIdCategory(idCategory)
+  }
+  
+  findByIdCategory(idCategory: number) {
+    this.categoryService.getByIdCategory(idCategory).subscribe((resp: CategoryModel) => {
+      this.category = resp
+    })
+  }
+
+  delete() {
+    this.categoryService.deleteCategory(this.idCategory).subscribe(() => {
+      alert('Categoria apagada com sucesso!')
+      this.router.navigate(['/category'])
+    })
   }
 
 }
